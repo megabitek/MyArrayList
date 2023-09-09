33dec3c85bcd6ce7795f12bf14a851635bfa2fca
Vladimir Bobrovskii <bobrovskiivs@gmail.com>
2023-09-10 01:27:35 +0400

    add comment and java docs

--- src/main/java/ru.lenabobr/MyArrayList.java
+++ src/main/java/ru.lenabobr/MyArrayList.java
@@ -1,128 +1,196 @@
-package  ru.lenabobr;
+package ru.lenabobr;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
-
-public class MyArrayList<T>{
+/** My implementation of an array list and methods for working with it*/
+public class MyArrayList<T> {
     /**
      * The array buffer into which the elements of the ArrayList are stored.
      * The capacity of the ArrayList is the length of this array buffer.
-     * Any empty ArrayList with elementData == DEFAULTSIZE_EMPTY will be expanded to currentSize when the first element is added*/
-    private T [] array;
+     * Any empty ArrayList with elementData == DEFAULTSIZE_EMPTY will be expanded to currentSize when the first element is added
+     */
+    private T[] array;
 
     /**
-     * The size of the ArrayList (the number of elements it contains).*/
+     * The size of the ArrayList (the number of elements it contains).
+     */
     private int currentSize;
+    /**
+     * Parametr of increase in array capacity when array reached the maximum size
+     */
+    private final int CAPACITY_DECREASE_PARAMETR = 2;
 
-    private final int DEFAULTSIZE_EMPTY=10;
+    /**
+     * Parametr specified limited difference between size and capacity
+     */
+    private final int SIZE_CAPACITY_DIFF = 3;
 
-    private  Comparator<T> comparator;
-/**
- *  This method allows you to install a comparator of a given type. it is needed when sorting an array is required
- *
- *  Type parameters:
- * <T> – the type of elements in this list*/
+    /**
+     * Default array capacity
+     */
+    private final int DEFAULT_CAPACITY_EMPTY = 10;
+
+    private Comparator<T> comparator;
+
+    /**
+     * This method allows you to install a comparator of a given type. it is needed when sorting an array.
+     * @param comparator - comparator for list's elements
+     *
+     */
     public void setComparator(Comparator<T> comparator) {
         this.comparator = comparator;
     }
-/**
- * */
 
-/**
- * Create new ArrayList with default capacity*/
-    public MyArrayList(){
-        array = (T[]) new Object [DEFAULTSIZE_EMPTY];
-        currentSize =0;
+    /**
+     * Create new ArrayList with default capacity
+     */
+    public MyArrayList() {
+        array = (T[]) new Object[DEFAULT_CAPACITY_EMPTY];
+        currentSize = 0;
     }
-/**
- * Create new ArrayList with user specified capacity
- * Throws:
- IllegalArgumentException – if the specified initial capacity is negative*/
-    public MyArrayList(int capacity){
-        if (capacity>0){
-        array = (T[]) new Object [capacity];
-        currentSize =0;}
-        else throw new IllegalArgumentException("Illegal Capacity: "+
+
+    /**
+     * Create new ArrayList with user specified capacity
+     *
+     * @param capacity- array capacity
+     * @throws  IllegalArgumentException – if the specified initial capacity is negative
+     */
+    public MyArrayList(int capacity) {
+        if (capacity > 0) {
+            array = (T[]) new Object[capacity];
+            currentSize = 0;
+        } else throw new IllegalArgumentException("Illegal Capacity: " +
                 capacity);
     }
-    public void addElement(T elem){
+
+    /**
+     * Add element with specified type in list
+     *
+     * @param elem - element for add in Array List
+     */
+    public void addElement(T elem) {
         array[currentSize] = elem;
         currentSize++;
     }
-    public void addElementByIndex(T elem, int index){
-        growSize(currentSize+1);
-        for (int i=currentSize; i>index; i--){
-            array[i]=array[i-1];
+
+    /**
+     * Add element in array list on the specified position.
+     * After inserting an element, the capacity is not enough,
+     * the array capacity is automatically increased (capacity *CAPACITY_DECREASE_PARAMETR)
+     *If the index goes beyond the array boundary, the original array is not modified
+     * @param elem - new element in array list
+     * @param index - index for new element
+     */
+    public void addElementByIndex(T elem, int index) {
+        checkIndex(index);
+        growSize(currentSize + 1);
+        for (int i = currentSize; i > index; i--) {
+            array[i] = array[i - 1];
         }
-         array[index]=elem;
+        array[index] = elem;
         currentSize++;
 
     }
-
-    public int getSize(){
+/**
+ * Return the current size of array
+ * @return - the array size (current element count)**/
+    public int getSize() {
         return currentSize;
     }
 
-    public T getElementByIndex(int index)  {
-        if (index <= currentSize)
+    /**Return the value of element for given position. If index is wrong return null.
+     * If the index goes beyond the array boundary, the original array is not modified
+     * @param index- index for getting element
+     * @return - the element in array list
+     * */
+    public T getElementByIndex(int index) {
+
+        if (checkIndex(index))
             return array[index];
         else return null;
     }
-
-    public void deleteElement(int index){
-        for (int i=index+1; i<currentSize; i++){
-            array[i-1]=array[i];
+/**Delete element on specified position from array list.
+ * If the index goes beyond the array boundary, the original array is not modified
+ @param index -  position of the element to be removed
+ * */
+    public void deleteElement(int index) {
+        if (checkIndex(index)){
+        for (int i = index + 1; i < currentSize; i++) {
+            array[i - 1] = array[i];
         }
-        array[currentSize-1]=null;
+        array[currentSize - 1] = null;
         currentSize--;
-    }
-    public void cleanAll(){
-        for(int i = 0; i < currentSize; i++) {
+    }}
+/**
+ * Delete all elements from array list*/
+    public void cleanAll() {
+        for (int i = 0; i < currentSize; i++) {
             array[i] = null;
-                    }
-        currentSize=0;
+        }
+        currentSize = 0;
     }
+
     /**
-     * this method use for array sorting */
-    public void sortArray (){
+     * Method is used to sort an array using quicksort algorithm. The field Comparator must be set.
+     */
+    public void sortArray() {
 
-     quickSort(array, 0, currentSize-1);
+        quickSort(array, 0, currentSize - 1);
     }
 
-    private void quickSort(T[] array, int start, int end){
-            if (start >=end) return;
-            int pivot = partition(array, 0, end, comparator);
-            quickSort(array, start, pivot-1 );
-            quickSort(array, pivot+1, end);
-        }
-        private  int partition (T [] array, int start, int end, Comparator <T>  comparator ){
-            T value = array[end];
-            int position = start;
-            for (int i = start;  i<end; i++){
-               if (comparator.compare(array[i], value) == 1) continue;
-                T temp = array[i];
-                array[i] = array[position];
-                array[position] = temp;
-                position++;
-            }
-            array[end]= array[position];
-            array[position] = value;
-            return position;
-        }
-
-    public void changeElement(T elem, int index){
-        array[index]=elem;
+    private void quickSort(T[] array, int start, int end) {
+        if (start >= end) return;
+        int pivot = partition(array, 0, end, comparator);
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
     }
 
-    private void growSize(int newSize){
-        if (array.length - newSize<3){
-            T [] newAr= (T[]) new Object [array.length*2];
+    private int partition(T[] array, int start, int end, Comparator<T> comparator) {
+        T value = array[end];
+        int position = start;
+        for (int i = start; i < end; i++) {
+            if (comparator.compare(array[i], value) == 1) continue;
+            T temp = array[i];
+            array[i] = array[position];
+            array[position] = temp;
+            position++;
+        }
+        array[end] = array[position];
+        array[position] = value;
+        return position;
+    }
+/**Change current element  value on the given position to new.
+ * If the index goes beyond the array boundary, the original array is not modified
+ * @param elem - the new  value of changing element in array list
+ * @param index - position of the changing element*/
+    public void changeElement(T elem, int index) {
+        if (checkIndex(index))
+        array[index] = elem;
+    }
+
+    private boolean checkIndex(int index){
+        if ((index <= currentSize)&(index>=0)) return true;
+        return false;
+    }
+
+    /**
+     * When array size reaches the specified size (capacity - size = SIZE_CAPACITY_DIFF),
+     * array capacity decrease with specified koefficient (old capacity*CAPACITY_DECREASE_PARAMETR)
+     *
+     * @param - array size for checking
+     */
+    private void growSize(int newSize) {
+        if (array.length - newSize < SIZE_CAPACITY_DIFF) {
+            T[] newAr = (T[]) new Object[array.length * CAPACITY_DECREASE_PARAMETR];
             newAr = Arrays.copyOf(array, array.length);
-            array= newAr;
+            array = newAr;
             newAr = null;
         }
     }
-    public void printArr(){
-        Arrays.stream(array).forEach(n -> System.out.println(n));
+/**
+ * Input all elements of array list on screen by line*/
+    public void printArr() {
+        Arrays.stream(Arrays.copyOf(array, currentSize)).forEach(n -> System.out.println(n));
     }
 }
--- src/main/test/ru/lena/bobr/MyArrayListTestInt.java
+++ src/main/test/ru/lena/bobr/MyArrayListTestInt.java
@@ -15,7 +15,7 @@ public class MyArrayListTestInt {
 
     @Before
     public void init() {
-    //    MyArrayList arrayList = new MyArrayList(10);
+        //    MyArrayList arrayList = new MyArrayList(10);
         arrayList.addElement(3);
         arrayList.addElement(2);
         arrayList.addElement(5);
@@ -23,45 +23,57 @@ public class MyArrayListTestInt {
     }
 
     @Test
-    public void negativeCapacity(){
+    public void negativeCapacity() {
         IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-        MyArrayList<Integer> arrayList1= new MyArrayList<>(-10);       });
+            MyArrayList<Integer> arrayList1 = new MyArrayList<>(-10);
+        });
         Assertions.assertEquals("Illegal Capacity: -10", thrown.getMessage());
     }
+
     @Test
-    public void getSize(){
-       // MyArrayList arrayList = new MyArrayList(10);
-        Assert.assertEquals(4,arrayList.getSize());
+    public void getSize() {
+        // MyArrayList arrayList = new MyArrayList(10);
+        Assert.assertEquals(4, arrayList.getSize());
     }
 
     @Test
     public void getElementByIndex() {
-
         Assert.assertEquals(5, arrayList.getElementByIndex(2));
-       }
+    }
+
+    @Test
+    public void getElementByWrongIndex() {
+        Assert.assertEquals(null, arrayList.getElementByIndex(8));
+    }
+
+    @Test
+    public void getElementByNegativeIndex() {
+        Assert.assertEquals(null, arrayList.getElementByIndex(-8));
+    }
 
 
     @Test
-     public void addElementByIndex(){
+    public void addElementByIndex() {
         arrayList.addElementByIndex(2, 2);
         Assert.assertEquals(5, arrayList.getSize());
         Assert.assertEquals(2, arrayList.getElementByIndex(2));
     }
+
     @Test
-    public void deleteElement(){
-    arrayList.deleteElement(3);
-    Assert.assertEquals(3, arrayList.getSize());
+    public void deleteElement() {
+        arrayList.deleteElement(3);
+        Assert.assertEquals(3, arrayList.getSize());
     }
 
-@Test
-    public void changeElement(){
+    @Test
+    public void changeElement() {
         Assert.assertEquals(5, arrayList.getElementByIndex(2));
         arrayList.changeElement(56, 2);
         Assert.assertEquals(56, arrayList.getElementByIndex(2));
     }
 
     @Test
-    public void sortArray(){
+    public void sortArray() {
         arrayList.setComparator(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
@@ -73,6 +85,39 @@ public class MyArrayListTestInt {
         Assert.assertEquals(3, arrayList.getElementByIndex(1));
         Assert.assertEquals(5, arrayList.getElementByIndex(2));
         Assert.assertEquals(8, arrayList.getElementByIndex(3));
+    }
+
+    @Test
+    public void sortArrayString(){
+
+        MyArrayList arrayListStr = new MyArrayList<String>();
+        arrayListStr.addElement("Tom");
+        arrayListStr.addElement("Jane");
+        arrayListStr.addElement("John");
+        arrayListStr.addElement("Pete");
+        arrayListStr.setComparator(new Comparator<String>() {
+            @Override
+            public int compare(String str1, String str2) {
+                if  (str1.compareTo(str2)==0)
+                    return 0;
+                else if (str1.compareTo(str2)>0)
+                    return 1;
+                else
+                    return -1;
+            }}
+        );
+
+        arrayListStr.sortArray();
+        Assert.assertEquals("Jane", arrayListStr.getElementByIndex(0));
+        Assert.assertEquals("John", arrayListStr.getElementByIndex(1));
+        Assert.assertEquals("Pete", arrayListStr.getElementByIndex(2));
+        Assert.assertEquals("Tom", arrayListStr.getElementByIndex(3));
+    }
+
+    @Test
+    public void cleanAll(){
+        arrayList.cleanAll();
+        Assert.assertEquals(0, arrayList.getSize());
     }
 }
 
